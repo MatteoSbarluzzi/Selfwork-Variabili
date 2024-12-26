0946d40afe97d62c50983d29c39efa7d8cfb4e1b
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2024-12-26 15:49:08 +0100

    Selfwork Variabili

---
+++ Selfwork variabili/main.js
@@ -0,0 +1,18 @@
+let num1 = 11;
+let num2 = 29;
+
+console.log( "Il valore della variabile num1 e' 11" );
+console.log( "Il valore della variabile num2 è 29" );
+
+let stringa = "Mi chiamo Matteo";
+console.log( "Il valore della variabile stringa e' Mi chiamo Matteo" );
+
+stringa = "Mi chiamo Antonio";
+console.log( "Il valore della variabile stringa e' Mi chiamo Antonio" );
+
+const PIGRECO = 3.14159265359;
+console.log ( "Il valore di PIGRECO e' 3.14159265359" );
+
+PIGRECO = 4.123456789;
+console.log ( "Il valore di PIGRECO e' 4.123456789");
+//main.js:16 Uncaught TypeError: Assignment to constant variable.
