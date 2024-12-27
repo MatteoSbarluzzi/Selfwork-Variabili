7e20ba05dc0a7b19c69c7ec308eafab327eb0b85
MatteoSbarluzzi <matteosbarluzzi1@gmail.com>
2024-12-27 12:41:31 +0100

    Selfwork Variabili corretto

---
+++ main.js
@@ -0,0 +1,15 @@
+let num1 = 11;
+let num2 = 29;
+
+console.log( "Il valore della variabile num1 e':" + num1);
+console.log( `Il valore della variabile num2 e' ${num2}`);
+
+let stringa = "Mi chiamo Matteo";
+stringa = "Mi chiamo Antonio";
+console.log( `Il valore della variabile stringa e' ${stringa}`);
+
+const pGreco = 3.14159265359;
+
+pGreco = 4.123456789;
+console.log ( `Il valore di pGreco e' ${pGreco}`);
+//main.js:16 Uncaught TypeError: Assignment to constant variable.
